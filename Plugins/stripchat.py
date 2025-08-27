f0b6f5912fd7a2c5e265481008627858ff3136d7
Narkhotik <nicopaille1@gmail.com>
2025-08-27 17:40:39 +0200

    Update stripchat.py

--- Plugins/stripchat.py
+++ Plugins/stripchat.py
@@ -1,23 +1,12 @@
 import re
+import random
+import json
+import os
 from streamlink.plugin import Plugin
 from streamlink.plugin.api import validate
 from streamlink.stream import HLSStream
 
 _url_re = re.compile(r"https?://(\w+\.)?stripchat\.com/(?P<username>[a-zA-Z0-9_-]+)")
-
-_post_schema = validate.Schema(
-    {
-        "cam": validate.Schema({
-                    'streamName' : validate.text,
-        }),
-        "user": validate.Schema({
-                    'user' : validate.Schema({
-                                'status' : validate.text,
-                                'isLive' : bool
-                    })
-        })    
-    }
-)
 
 
 class Stripchat(Plugin):
@@ -26,32 +15,83 @@ class Stripchat(Plugin):
         return _url_re.match(url)
 
     def _get_streams(self):
-        self.session.http.cookies.set("stripchat_com_sessionId", "YOUR_SESSION_ID")
-        match = _url_re.match(self.url)
-        username = match.group("username")
-        api_call = "https://stripchat.com/api/front/v2/models/username/{0}/cam".format(username)
-        headers = {
-            "Content-Type": "application/x-www-form-urlencoded",
-            "X-Requested-With": "XMLHttpRequest",
-            "Referer": self.url,
-        }
+        data = self.session.http.json(
+            self.session.http.get(
+                "https://stripchat.com/api/front/v2/models/username/{0}/cam".format(
+                    _url_re.match(self.url).group("username")
+                ),
+                headers={
+                    "Content-Type": "application/x-www-form-urlencoded",
+                    "X-Requested-With": "XMLHttpRequest",
+                    "Referer": self.url,
+                },
+            )
+        )
+        with open(
+            os.path.dirname(os.path.realpath(__file__)) + "/debug.json", "w"
+        ) as fp:
+            fp.write(json.dumps(data, indent=4))
 
-        res = self.session.http.get(api_call, headers=headers)
-
-        data = self.session.http.json(res, schema=_post_schema)
-        
-        server = "https://b-hls-01.doppiocdn.com/hls/{0}/master_{0}.m3u8".format(data["cam"]["streamName"])
-
-        server0 = "https://b-hls-01.doppiocdn.com/hls/{0}/{0}.m3u8".format(data["cam"]["streamName"])
-
+        self.logger.info("Stream live: {0}".format(data["user"]["user"]["isLive"]))
         self.logger.info("Stream status: {0}".format(data["user"]["user"]["status"]))
+        self.logger.info(
+            "Qualities available: {0}".format(
+                ", ".join(data["cam"]["broadcastSettings"]["presets"]["default"])
+            )
+        )
 
-        if (data["user"]["user"]["isLive"] is True and data["user"]["user"]["status"] == "public" and server):
+        num = random.choice(
+            [
+                "01",
+                "02",
+                "03",
+                "04",
+                "05",
+                "06",
+                "07",
+                "08",
+                "09",
+                "10",
+                "11",
+                "12",
+                "13",
+                "14",
+                "15",
+                "16",
+                "17",
+                "18",
+                "19",
+                "20",
+                "21",
+                "22",
+                "23",
+                "24",
+            ]
+        )
+
+        # https://edge-hls.doppiocdn.live/hls/62310556/master/62310556_auto.m3u8?playlistType=lowLatency
+        # https://media-hls.doppiocdn.net/b-hls-10/62310556/62310556_1080p.m3u8?psch=v1&pkey=Zokee2OhPh9kugh4&playlistType=lowLatency
+        # headers = {
+        #     "Referer": self.url,
+        #     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:142.0) Gecko/20100101 Firefox/142.0",
+        # }
+        # cookies = {"stripchat_com_sessionId": "...", "other cookie": "..."}
+
+        if data["user"]["user"]["isLive"] is True:
             try:
-                for s in HLSStream.parse_variant_playlist(self.session,server,headers={'Referer': self.url}).items():
+                for s in HLSStream.parse_variant_playlist(
+                    self.session,
+                    "https://edge-hls.doppiocdn.live/hls/{0}/master/{0}_auto.m3u8".format(data["cam"]["streamName"]), # fmt: skip
+                ).items():
+                    self.logger.info("Stream: {0}".format(s))
                     yield s
             except IOError as err:
-                stream = HLSStream(self.session, server0)
+                stream = HLSStream(
+                    self.session,
+                    "https://media-hls.doppiocdn.live/b-hls-{0}/{1}/{1}_{2}.m3u8".format(num, data["cam"]["streamName"], data["cam"]["broadcastSettings"]["presets"]["default"][0]), # fmt: skip
+                )
+                self.logger.info("Stream: {0}".format(stream))
                 yield "Auto", stream
+
 
 __plugin__ = Stripchat
